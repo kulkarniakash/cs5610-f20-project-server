9c9027e170c6d8d6dd4ae4957484352069f0918d
kulkarniakash <kulkarni.akash@gmail.com>
2020-11-02 20:42:48 -0500

    Java server to communicate with Spotify auth

---
+++ src/main/java/com/example/cs5610f20projectserver/controller/SpotifyAuthController.java
@@ -0,0 +1,29 @@
+package com.example.cs5610f20projectserver.controller;
+
+import com.example.cs5610f20projectserver.Model.AuthToken;
+import com.example.cs5610f20projectserver.Service.SpotifyServices;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+//change origins later!
+@RestController
+//change to heroku link
+@CrossOrigin(origins = "*")
+public class SpotifyAuthController {
+    private String clientId;
+    private String clientSecret;
+
+    public SpotifyAuthController() {
+        clientId = "08b8c56399c848388c05769966c722e2";
+        clientSecret = "d08df511bc6749b0b374749fb4c25321";
+    }
+
+    @PostMapping("/authaccess")
+    public @ResponseBody String getToken(@RequestParam("code") String code) throws IOException, InterruptedException {
+        String encodedData = Base64.getEncoder().encodeToString((clientId + ':' + clientSecret).getBytes(StandardCharsets.UTF_8));
+        return SpotifyServices.getTokens(code, encodedData);
+    }
+}
